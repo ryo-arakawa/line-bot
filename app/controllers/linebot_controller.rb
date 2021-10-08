01987de858b112b2aa7efc59ad2e8a62cb1d1fd5
Ryo Arakawa <67402912+ryo-arakawa@users.noreply.github.com>
2021-10-08 23:52:56 +0900

    fix word comment

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -65,6 +65,7 @@ class LinebotController < ApplicationController
               word =
                 ["雨だけど元気出していこうね！",
                  "雨に負けずファイト！！",
+                 "雨だけどきっといいことあるよ！",
                  "雨だけどあなたの明るさでみんなを元気にしてあげて(^^)"].sample
               push =
                 "今日の天気？\n今日は雨が降りそうだから傘があった方が安心だよ。\n　  6〜12時　#{per06to12}％\n　12〜18時　 #{per12to18}％\n　18〜24時　#{per18to24}％\n#{word}"
